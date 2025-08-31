3e19f1cb057eacc9ea1395e89efecd1b4264b9c7
miihgonzaga <miriam.gonzaga@ufpe.br>
2025-08-31 14:44:29 -0300

    primeiro commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# aprendendo versionamento de código
+'''
+    oiee!!
+    vamos tentar subir esse código simples 
+    de Hello World pro nosso repositório remoto :)
+
+'''
+
+mensagem = "Hello World"
+print(mensagem)
